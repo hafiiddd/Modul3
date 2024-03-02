71c376eab38a59d6ee61b8fe5e033477de2d29a6
hafiiddd <hafid.akhyar0411@gmail.com>
2024-03-02 23:44:44 +0700

    tambahan fitur GUI

---
+++ modul3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modul3
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Halo,"+name;
+            textBox2.Show();
+        }
+    }
+}
